d54e75ff5dd0b2472f885f41451f71d9840ce8d8
Mario Alexander Vargas Celis <41644949+celioso@users.noreply.github.com>
2025-11-01 11:39:45 -0500

    Actualizar models.py

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,7 +1,7 @@
-from django.db import models
+7from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+#324
 class PublishedManager(models.Manager):
     def get_queryset(selt):
         return super().get_queryset().filter(status=Post.Status.PUBLISHED)
