60145d73e048ebb377fabd26851ef0d76d68dcdd
Mario Celis <celioso1@hotmail.com>
2025-11-26 20:51:35 -0500

    Installing PostgreSQL

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostFeed
 
 app_name = 'blog'
 
@@ -12,4 +13,5 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
         views.post_detail,
         name='post_detail'),
+    path('feed/',LatestPostFeed(), name='post_feed'),
 ]
